16473443e916731559530262d268abd16d604887
SorsDev <sorsromerze@gmail.com>
2021-05-19 11:34:56 -0500

    Header Complete

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from './components/Header';
+const App = () => {
+  return (
+    <div className="contenedor">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
